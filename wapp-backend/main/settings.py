21f24cbb8d039158cf7ec8fd87adb97a8b92de62
slavvok <slavvok@gmail.com>
2020-04-15 23:45:54 +0300

    Token's expiration time increase

--- wapp-backend/main/settings.py
+++ wapp-backend/main/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import datetime
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,9 +29,9 @@ DEBUG = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
-# JWT_AUTH = {
-#     'JWT_ALLOW_REFRESH': True,
-# }
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(minutes=40),
+}
 
 # Application definition
 
